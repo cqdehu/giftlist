6ab670a69aa51d659f5d03e123854bdc59211090
Lehotaiabel <cqde.hu@gmail.com>
2023-04-09 18:09:01 +0200

    auth Token system

--- script.js
+++ script.js
@@ -1,31 +1,43 @@
 function auth() {
+    // Check if JWT token exists in cookies
+    const jwtToken = getCookie('jwt_token');
+  
+    // If token does not exist, redirect to login page
+    if (!jwtToken) {
+      window.location.href = "/login.html";
+      return;
+    }
+  
+    // Send AJAX request to server to verify JWT token
     $.ajax({
-        type: "POST",
-        url: "auth.php",
-        success: function() {
-            console.log("Érvényes Token.")
-        },
-        error: function() {
-            console.log("Érvénytelen Token.")
+      type: "POST",
+      url: "verify_token.php",
+      data: { token: jwtToken },
+      success: function(response) {
+        // If token is valid, proceed to the authenticated page
+        if (response === "valid") {
+          window.location.href = "/home.html";
         }
-    })
-}
-    
-$(document).ready(function(){
-    $("#logoutBtn").click(function(){
-        $.ajax({
-            type: "POST",
-            url: "logout.php",
-            success: function() {
-                console.log("A jwt_token cookie sikeresen törölve lett!");
-                
-            },
-            error: function() {
-                console.log("Hiba történt a jwt_token cookie törlésekor!");
-                
-            }
-        });
-        auth()
-    })
-})
+        // If token is invalid, redirect to login page
+        else {
+          window.location.href = "/login.html";
+        }
+      },
+      error: function() {
+        // If an error occurs, redirect to login page
+        window.location.href = "/login.html";
+      }
+    });
+  }
+  
+  // Helper function to get cookie value by name
+  function getCookie(name) {
+    const value = "; " + document.cookie;
+    const parts = value.split("; " + name + "=");
+    if (parts.length === 2) {
+      return parts.pop().split(";").shift();
+    }
+  }
+  
 
+auth()
