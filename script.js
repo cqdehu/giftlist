218b699038933b096ff41e76cf6370d08330b9bd
Lehotaiabel <cqde.hu@gmail.com>
2023-04-09 18:22:49 +0200

    auth Token system

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ function getCookie(name) {
     const value = `; ${document.cookie}`;
     const parts = value.split(`; ${name}=`);
     if (parts.length === 2) return parts.pop().split(';').shift();
-    console.log(document.cookie)
+    console.log(name)
 }
 
 
