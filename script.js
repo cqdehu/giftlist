5890c1cddb1e4b5d36fdaaa8849999316072348a
Lehotaiabel <cqde.hu@gmail.com>
2023-04-09 15:41:59 +0200

    auth Token system

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 function auth() {
+    var token = getCookie('jwt_token'); // JWT lekérése a süti értékéből
     $.ajax({
         type: "POST",
         url: "auth.php",
+        headers: {
+            "Authorization": "Bearer " + token // JWT átadása az Authorization fejlécben
+        },
         success: function() {
             console.log("Érvényes Token.")
         },
@@ -10,6 +14,22 @@ function auth() {
         }
     })
 }
+
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = cookies[i].trim();
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
 
 auth()
 
