c11f18fd18d5436e308ff6316c3ce5c2088ef2c9
Lehotaiabel <cqde.hu@gmail.com>
2023-04-10 18:45:24 +0200

    Cookie Token

---
+++ logout.php
@@ -0,0 +1,10 @@
+<?php
+session_start();
+// Töröljük az összes munkamenet változót
+session_unset();
+// Töröljük a munkamenetet
+session_destroy();
+// Átirányítjuk a felhasználót a bejelentkezési oldalra
+header("Location: login.php");
+exit();
+?>
