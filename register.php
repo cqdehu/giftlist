915b46c498eeaaffb99c1226123c0c30a5cc2005
Lehotaiabel <cqde.hu@gmail.com>
2023-04-10 18:33:04 +0200

    Cookie Token

---
+++ register.php
@@ -0,0 +1,49 @@
+<?php
+session_start();
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  // Ellenőrizzük, hogy a felhasználónév és a jelszó mezők nem üresek
+  if (!empty($_POST['username']) && !empty($_POST['password'])) {
+    // Kapcsolódás az adatbázishoz
+    $servername = 'localhost';
+    $dbusername = 'u142909563_admin';
+    $dbpassword = 'kcRN[bK7';
+    $dbname = 'u142909563_database';
+
+    $conn = mysqli_connect($servername, $dbusername, $dbpassword, $dbname);
+
+    // Ellenőrizzük, hogy sikerült-e kapcsolódni az adatbázishoz
+    if (!$conn) {
+      die("Connection failed: " . mysqli_connect_error());
+    }
+
+    // Ellenőrizzük, hogy a felhasználónév még nem foglalt-e
+    $username = $_POST['username'];
+    $query = "SELECT * FROM users WHERE username='$username'";
+    $result = mysqli_query($conn, $query);
+
+    if (mysqli_num_rows($result) == 0) {
+      // Hasheljük a jelszót
+      $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+      // Adjuk hozzá az új felhasználót az adatbázishoz
+      $query = "INSERT INTO users (username, password) VALUES ('$username', '$password')";
+      $result = mysqli_query($conn, $query);
+
+      if ($result) {
+        // Sikeres regisztráció, átirányítjuk a felhasználót az üdvözlőoldalra
+        $_SESSION['username'] = $username;
+        header("Location: welcome.php");
+        exit();
+      } else {
+        echo "Hiba történt a regisztráció során.";
+      }
+    } else {
+      echo "A felhasználónév már foglalt.";
+    }
+
+    mysqli_close($conn);
+  } else {
+    echo "Kérjük, töltse ki mindkét mezőt.";
+  }
+}
+?>
