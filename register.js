cfa063c65f05c6348ffe50954c234b0d7981d7fd
Lehotaiabel <cqde.hu@gmail.com>
2023-04-10 22:19:28 +0200

    Cookie Token

---
+++ register.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+    $("#registration-form").submit(function (e) {
+        e.preventDefault(); // megakadályozza az alapértelmezett űrlap beküldését
+
+        var username = $("#username").val();
+        var password = $("#password").val();
+
+        $.ajax({
+            type: "POST",
+            url: "register.php",
+            data: { username: username, password: password },
+            success: function (response) {
+                console.log(response);
+                if (response == "success") {
+                    // sikeres bejelentkezés, átirányítás a welcome.html oldalra
+                    window.location.replace("home.html");
+                } else {
+                    // hibaüzenet megjelenítése
+                    $("#error-message").text(response);
+                }
+            },
+            error: function () {
+                // Hibás AJAX hívás esetén kezeljük a hibát
+                $("#message").html("Hiba történt az AJAX hívás során.");
+            }
+        });
+    });
+});
