a1105b419374abcc203912aa03b7711d04cf2135
Lehotaiabel <cqde.hu@gmail.com>
2023-04-10 21:53:25 +0200

    Cookie Token

--- script.js
+++ script.js
@@ -60,26 +60,5 @@ $(document).ready(function () {
 
 ////
 
-$(document).ready(function() {
-    // AJAX hívás küldése a szervernek
-    $.ajax({
-      type: "GET",
-      url: "check_session.php",
-      success: function(data) {
-        // Sikeres válasz esetén kezeljük a visszatérő adatot
-        if (data == "success") {
-          // A felhasználó be van jelentkezve, megjelenítjük a tartalmat
-          console.log("A felhasználó be van jelentkezve!")
-        } else {
-          // A felhasználó nincs bejelentkezve, megjelenítjük az üzenetet
-          console.log("A felhasználó nincs bejelentkezve!")
-          window.location.replace("login.html");
-        }
-      },
-      error: function() {
-        // Hibás AJAX hívás esetén kezeljük a hibát
-        $("#message").html("Hiba történt az AJAX hívás során.");
-      }
-    });
-  });  
+
 
