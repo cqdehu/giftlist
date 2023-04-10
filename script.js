9b9a2fbe8fe28291dd8027d1c35e32f9877cdfa0
Lehotaiabel <cqde.hu@gmail.com>
2023-04-10 19:46:30 +0200

    Cookie Token

--- script.js
+++ script.js
@@ -1,25 +1,46 @@
-$(document).ready(function(){
-    $("#login-form").submit(function(e){
-      e.preventDefault(); // megakadályozza az alapértelmezett űrlap beküldését
-  
-      var username = $("#username").val();
-      var password = $("#password").val();
-  
-      $.ajax({
-        type: "POST",
-        url: "login.php",
-        data: {username: username, password: password},
-        success: function(response){
-          console.log(response);
-          if (response == "success") {
-            // sikeres bejelentkezés, átirányítás a welcome.html oldalra
-            window.location.replace("welcome.html");
-          } else {
-            // hibaüzenet megjelenítése
-            $("#error-message").text(response);
-          }
-        }
-      });
+$(document).ready(function () {
+    $("#login-form").submit(function (e) {
+        e.preventDefault(); // megakadályozza az alapértelmezett űrlap beküldését
+
+        var username = $("#username").val();
+        var password = $("#password").val();
+
+        $.ajax({
+            type: "POST",
+            url: "login.php",
+            data: { username: username, password: password },
+            success: function (response) {
+                console.log(response);
+                if (response == "success") {
+                    // sikeres bejelentkezés, átirányítás a welcome.html oldalra
+                    window.location.replace("welcome.html");
+                } else {
+                    // hibaüzenet megjelenítése
+                    $("#error-message").text(response);
+                }
+            }
+        });
     });
-  });
-  
+});
+
+$(document).ready(function () {
+    // Elkapjuk a regisztrációs űrlap küldési eseményét
+    $('#registration-form').on('submit', function (e) {
+        e.preventDefault(); // Megakadályozzuk az alapértelmezett űrlapküldési viselkedést
+
+        // Lekérjük az űrlap adatait
+        var formData = $(this).serialize();
+
+        // Elküldjük az adatokat a szervernek Ajax kéréssel
+        $.ajax({
+            url: 'register.php',
+            type: 'POST',
+            data: formData,
+            success: function (response) {
+                // Megjelenítjük a szerver válaszát
+                $('#response').html(response);
+            }
+        });
+    });
+});
+
