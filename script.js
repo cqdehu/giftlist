da0a93015237216edbed07335523a9596e3d272c
Lehotaiabel <cqde.hu@gmail.com>
2023-04-10 20:07:50 +0200

    Cookie Token

--- script.js
+++ script.js
@@ -13,37 +13,10 @@ $(document).ready(function () {
                 console.log(response);
                 if (response == "success") {
                     // sikeres bejelentkezés, átirányítás a welcome.html oldalra
-                    window.location.replace("welcome.html");
+                    window.location.replace("welcome.php");
                 } else {
                     // hibaüzenet megjelenítése
                     $("#error-message").text(response);
-                }
-            }
-        });
-    });
-});
-
-$(document).ready(function () {
-    $("#registration-form").click(function () {
-        var username = $("#username").val();
-        var password = $("#password").val();
-
-        // AJAX hívás küldése a szervernek
-        $.ajax({
-            type: "POST",
-            url: "register.php",
-            data: {
-                username: username,
-                password: password
-            },
-            success: function (data) {
-                // Sikeres válasz esetén kezeljük a visszatérő adatot
-                if (data == "success") {
-                    // Sikeres regisztráció, átirányítjuk a felhasználót az üdvözlőoldalra
-                    window.location.href = "welcome.php";
-                } else {
-                    // Hibás regisztráció, megjelenítjük az üzenetet
-                    $("#message").html(data);
                 }
             },
             error: function () {
@@ -53,5 +26,69 @@ $(document).ready(function () {
         });
     });
 });
+
+//
+
+$(document).ready(function () {
+    $("#registration-form").submit(function (e) {
+        e.preventDefault(); // megakadályozza az alapértelmezett űrlap beküldését
+
+        var username = $("#username").val();
+        var password = $("#password").val();
+
+        $.ajax({
+            type: "POST",
+            url: "register.php",
+            data: { username: username, password: password },
+            success: function (response) {
+                console.log(response);
+                if (response == "success") {
+                    // sikeres bejelentkezés, átirányítás a welcome.html oldalra
+                    window.location.replace("welcome.php");
+                } else {
+                    // hibaüzenet megjelenítése
+                    $("#error-message").text(response);
+                }
+            },
+            error: function () {
+                // Hibás AJAX hívás esetén kezeljük a hibát
+                $("#message").html("Hiba történt az AJAX hívás során.");
+            }
+        });
+    });
+});
+
+////
+
+//$(document).ready(function () {
+//    $("#registration-form").click(function () {
+//        var username = $("#username").val();
+//        var password = $("#password").val();
+//
+//        // AJAX hívás küldése a szervernek
+//        $.ajax({
+//            type: "POST",
+//            url: "register.php",
+//            data: {
+//                username: username,
+//                password: password
+//            },
+//            success: function (data) {
+//                // Sikeres válasz esetén kezeljük a visszatérő adatot
+//                if (data == "success") {
+//                    // Sikeres regisztráció, átirányítjuk a felhasználót az üdvözlőoldalra
+//                    window.location.href = "welcome.php";
+//                } else {
+//                    // Hibás regisztráció, megjelenítjük az üzenetet
+//                    $("#message").html(data);
+//                }
+//            },
+//            error: function () {
+//                // Hibás AJAX hívás esetén kezeljük a hibát
+//                $("#message").html("Hiba történt az AJAX hívás során.");
+//            }
+//        });
+//    });
+//});
 
 
