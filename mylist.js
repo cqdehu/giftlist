354da93d8f594005b2fbb4a82b455f8fa859e71e
Lehotaiabel <cqde.hu@gmail.com>
2023-04-11 12:53:45 +0200

    login

--- mylist.js
+++ mylist.js
@@ -89,7 +89,7 @@ function loadItem() {
 
 
             }
-            if (data != "failed") {
+            if (data) {
                 console.log("A lista elemei sikeresen betöltődtek!")
                 console.log(data)
             } else {
