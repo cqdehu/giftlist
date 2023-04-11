861d9d50158b42592552c72daacbcf04569fc463
Lehotaiabel <cqde.hu@gmail.com>
2023-04-11 18:01:24 +0200

    update

--- updateitem.php
+++ updateitem.php
@@ -28,7 +28,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     if (mysqli_num_rows($result) == 0) {
       // Az itemet a felhasználói adatbázisban frissítjük
-      $query = "UPDATE `items` SET `name`='$name',`status`='$status',`createDate`='$createDate' WHERE `id`='$id' AND `name` = '$name' AND `user` = '$user'";
+      $query = "UPDATE `items` SET `name`='$name',`status`='$status',`createDate`='$createDate' WHERE `id`='$id' AND `user` = '$user' AND `name` = '$name'";
       $result = mysqli_query($conn, $query);
 
       if ($result) {
@@ -45,5 +45,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     echo "Kérjük, töltse ki az Item mezőt.";
   }
 }
+
 
 ?>
