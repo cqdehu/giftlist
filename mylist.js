3f57c1a3ffcf3855d84b387da56cc3ed218fc87e
Lehotaiabel <cqde.hu@gmail.com>
2023-04-11 16:34:56 +0200

    update

--- mylist.js
+++ mylist.js
@@ -86,6 +86,12 @@ function loadItem() {
                     newItemStatus.src = ""
                 }
 
+                //OldItem
+                //var today = new Date();
+                //if (today.getFullYear() - createDate.getFullYear() >= 3){
+                //    console.log("Régi elem szerepel a listán.")
+                //}
+
 
                 listItems.appendChild(newCardDiv)
                 newCardDiv.appendChild(newItemNameDiv)
@@ -256,4 +262,61 @@ $('#deleteItemBtn').on('click', function () {
 
 
 });
+
+
+$(document).ready(function() {
+    $("#updateItemBtn").click(function() {
+  
+      // Ellenőrizze, hogy van-e kiválasztott elem
+      if (lastClickedCard == null) {
+        toastText.innerHTML = `Nincs kiválsztott elem!`;
+        alertToast.show();
+        return;
+      }
+  
+      // Ellenőrizze, hogy az elem neve ki van-e töltve
+      if (newItemName.value == "") {
+        var editItemNameInputValidation = document.querySelector("#editItemNameInputValidation");
+        editItemNameInputValidation.innerHTML = "Az Item mező kitöltése kötelező!";
+        return;
+      }
+  
+      // Törölje az elem nevére vonatkozó validációs üzenetet, ha van beírva név
+      if (newItemName.value !== "") {
+        var editItemNameInputValidation = document.querySelector("#editItemNameInputValidation");
+        editItemNameInputValidation.innerHTML = "";
+      }
+  
+      // Ellenőrizze, hogy az elem neve még nem szerepel a listában
+      if (items.includes(newItemName.value)) {
+        toastText.innerHTML = `${newItemName.value} elem már szerepel a listádon!`;
+        alertToast.show();
+        return;
+      }
+  
+      // Elküldjük az adatokat a PHP szkriptnek Jquery Ajax segítségével
+      $.ajax({
+        type: "POST",
+        url: "update-item.php",
+        data: {
+          name: newItemName.value,
+          status: newStatus.value,
+          user: "<?php echo $_SESSION['username']; ?>",
+          createDate: createDate,
+          id: "<?php echo $lastClickedCard.id; ?>"
+        },
+        success: function(result) {
+          if (result === "success") {
+            // Sikeres esetben frissítjük az oldalt
+            location.reload();
+          } else {
+            // Hiba esetén kiírjuk a hibaüzenetet
+            toastText.innerHTML = result;
+            alertToast.show();
+          }
+        }
+      });
+    });
+  });
+  
 
---
+++ updateitem.php
@@ -0,0 +1,49 @@
+<?php
+session_start();
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  // Ellenőrizzük, hogy az item név mező nem üres
+  if (!empty($_POST['name'])) {
+    // Kapcsolódás az adatbázishoz
+    $servername = 'localhost';
+    $dbusername = 'u142909563_admin';
+    $dbpassword = 'kcRN[bK7';
+    $dbname = 'u142909563_database';
+
+    $conn = mysqli_connect($servername, $dbusername, $dbpassword, $dbname);
+
+    // Ellenőrizzük, hogy sikerült-e kapcsolódni az adatbázishoz
+    if (!$conn) {
+      die("Connection failed: " . mysqli_connect_error());
+    }
+
+    // Ellenőrizzük, hogy az item neve még nem foglalt-e
+    $name = $_POST['name'];
+    $user = $_SESSION['username'];
+    $status = $_POST['status'];
+    $createDate = $_POST['createDate'];
+    $id = $_SESSION['id'];
+
+    $query = "SELECT * FROM `items` WHERE `name` = '$name' AND `user` = '$user' AND `id` != '$id'";
+    $result = mysqli_query($conn, $query);
+
+    if (mysqli_num_rows($result) == 0) {
+      // Az itemet a felhasználói adatbázisban frissítjük
+      $query = "UPDATE `items` SET `name`='$name',`status`='$status',`createDate`='$createDate' WHERE `id`='$id'";
+      $result = mysqli_query($conn, $query);
+
+      if ($result) {
+        echo "success";
+      } else {
+        echo "Hiba történt a tétel módosítása során.";
+      }
+    } else {
+      echo "Már egyszer szerepel a listán.";
+    }
+
+    mysqli_close($conn);
+  } else {
+    echo "Kérjük, töltse ki az Item mezőt.";
+  }
+}
+
+?>
