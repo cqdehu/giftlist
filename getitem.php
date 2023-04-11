7dba9e2e040b1741e0118b24e5f7048f2f69c925
Lehotaiabel <cqde.hu@gmail.com>
2023-04-11 03:04:07 +0200

    mylist update

--- getitem.php
+++ getitem.php
@@ -1,38 +1,40 @@
 <?php
 
-header('Content-Type: application/json');
+session_start();
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    header('Content-Type: application/json');
 
-// Ellenőrizzük, hogy a felhasználónév és a jelszó mezők nem üresek
-// Kapcsolódás az adatbázishoz
-$servername = 'localhost';
-$dbusername = 'u142909563_admin';
-$dbpassword = 'kcRN[bK7';
-$dbname = 'u142909563_database';
+    // Ellenőrizzük, hogy a felhasználónév és a jelszó mezők nem üresek
+    // Kapcsolódás az adatbázishoz
+    $servername = 'localhost';
+    $dbusername = 'u142909563_admin';
+    $dbpassword = 'kcRN[bK7';
+    $dbname = 'u142909563_database';
 
-$conn = mysqli_connect($servername, $dbusername, $dbpassword, $dbname);
+    $conn = mysqli_connect($servername, $dbusername, $dbpassword, $dbname);
 
-// Ellenőrizzük, hogy sikerült-e kapcsolódni az adatbázishoz
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-
-// Ellenőrizzük, hogy a felhasználónév még nem foglalt-e
-$user = $_SESSION['username'];
-
-$sql = "SELECT `name`, `status`, `user`, `createDate` FROM `items` WHERE `user` = $user";
-$result = mysqli_query($conn, $sql);
-
-if (mysqli_num_rows($result) > 0) {
-    // Az eredményt tömbbe mentjük
-    $items = array();
-    while ($row = mysqli_fetch_assoc($result)) {
-        $items[] = $row;
+    // Ellenőrizzük, hogy sikerült-e kapcsolódni az adatbázishoz
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
     }
-} else {
-    echo "Nincs eredmény";
+
+    // Ellenőrizzük, hogy a felhasználónév még nem foglalt-e
+    $user = $_SESSION['username'];
+
+    $sql = "SELECT `name`, `status`, `user`, `createDate` FROM `items` WHERE `user` = $user";
+    $result = mysqli_query($conn, $sql);
+
+    if (mysqli_num_rows($result) > 0) {
+        // Az eredményt tömbbe mentjük
+        $items = array();
+        while ($row = mysqli_fetch_assoc($result)) {
+            $items[] = $row;
+        }
+    } else {
+        echo "Nincs eredmény";
+    }
+
+    echo json_encode($items);
+
+    mysqli_close($conn);
 }
-
-echo json_encode($items);
-
-mysqli_close($conn);
-?>
