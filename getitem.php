73bb84d28ded56364ab006db255e40a430a2c2dd
Lehotaiabel <cqde.hu@gmail.com>
2023-04-23 22:07:06 +0200

    invite system

--- getitem.php
+++ getitem.php
@@ -21,7 +21,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $user = $_SESSION['username'];
     $userto = $_POST['userto'];
 
-    $sql = "SELECT `name`, `status`, `user`, `createDate`, `id`, `userto` FROM `items` WHERE `user` = '$user' AND `userto` = '$userto' OR `user`=`userto`";
+    $sql = "SELECT `name`, `status`, `user`, `createDate`, `id`, `userto` FROM `items` WHERE `user` = '$user' AND `userto` = '$userto'";
     $result = mysqli_query($conn, $sql);
 
     if (mysqli_num_rows($result) > 0) {
