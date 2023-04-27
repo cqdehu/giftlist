b3b87679d4fcf305d08b382824bc7119154328ba
Lehotaiabel <cqde.hu@gmail.com>
2023-04-27 20:20:31 +0200

    invite system

--- deleteitem.php
+++ deleteitem.php
@@ -1,8 +1,8 @@
 <?php
-
 session_start();
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-
+  // Ellenőrizzük, hogy az item név mező nem üres
+  if (!empty($_POST['selectedItem'])) {
     // Kapcsolódás az adatbázishoz
     $servername = 'localhost';
     $dbusername = 'u142909563_admin';
@@ -13,37 +13,33 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     // Ellenőrizzük, hogy sikerült-e kapcsolódni az adatbázishoz
     if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
+      die("Connection failed: " . mysqli_connect_error());
     }
 
-    // Ellenőrizzük, hogy a felhasználó be van-e jelentkezve
-    if (!isset($_SESSION['username'])) {
-        die("A művelet végrehajtásához be kell jelentkezned.");
-    }
-
-    // Ellenőrizzük, hogy a POST kérésben meg van-e adva a törlendő elem azonosítója
-    if (!isset($_POST['itemId'])) {
-        die("Nincs megadva az elem azonosítója.");
-    }
-
-    // Ellenőrizzük, hogy a felhasználóhoz tartozik-e az adott elem
-    $itemId = $_POST['itemId'];
+    // Ellenőrizzük, hogy az item neve a felhasználóhoz tartozik-e
     $user = $_SESSION['username'];
-    $sql = "SELECT * FROM `items` WHERE `name` = '$itemId' AND `user` = '$user'";
-    $result = mysqli_query($conn, $sql);
+    $userto = $_POST['userto'];
+    $selectedItem = $_POST['selectedItem'];
+    $query = "SELECT * FROM `items` WHERE `name` = '$selectedItem' AND `user` = '$user' AND `userto` = '$userto'";
+    $result = mysqli_query($conn, $query);
 
-    if (mysqli_num_rows($result) == 0) {
-        die("A törlendő elem nem található, vagy nem tartozik hozzád.");
+    if (mysqli_num_rows($result) == 1) {
+      // Az itemet töröljük a felhasználói adatbázisból
+      $query = "DELETE FROM `items` WHERE `user` = '$user' AND `userto` = '$userto' AND `name` = '$selectedItem'";
+      $result = mysqli_query($conn, $query);
+
+      if ($result) {
+        echo "success";
+      } else {
+        echo "Hiba történt a tétel törlése során.";
+      }
+    } else {
+      echo "Csak saját elemek törölhetőek.";
     }
 
-    // Töröljük az elemet az adatbázisból
-    $sql = "DELETE FROM `items` WHERE `name` = '$itemId' AND `user` = '$user'";
-    if (!mysqli_query($conn, $sql)) {
-        die("Nem sikerült törölni az elemet az adatbázisból.");
-    }
-
-    // Sikeres törlés esetén visszatérünk a főoldalra
+    mysqli_close($conn);
+  } else {
+    echo "Kérjük, válassza ki az Item mezőből a törölni kívánt elemet.";
+  }
 }
-
-
 ?>
