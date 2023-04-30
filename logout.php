7cd370853386f855783aeb0af213238944a7edc5
Lehotaiabel <cqde.hu@gmail.com>
2023-04-30 19:51:37 +0200

    refresh

--- logout.php
+++ logout.php
@@ -4,6 +4,16 @@ session_start();
 session_unset();
 // Töröljük a munkamenetet
 session_destroy();
+
+// Törlés a session-tárolóból
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
+
 // Átirányítjuk a felhasználót a bejelentkezési oldalra
 setcookie('PHPSESSID', '', time()-3600);
 
