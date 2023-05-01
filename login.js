1903c3b131280c36ee1a421450bdeb4d980a74f3
Lehotaiabel <cqde.hu@gmail.com>
2023-05-01 19:46:00 +0200

    refresh

--- login.js
+++ login.js
@@ -30,7 +30,6 @@ $(document).ready(function () {
                 console.log(response);
                 if (response == "success") {
                     // sikeres bejelentkezés, átirányítás a welcome.html oldalra
-                    $.cookie("selectedUser",username)
                     window.location.replace("mylist.html");
                     
                 } else {
