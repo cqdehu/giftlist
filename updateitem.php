b675fc0690edb8e33e30ae099af2f3ef9b367612
Lehotaiabel <cqde.hu@gmail.com>
2023-05-01 19:18:05 +0200

    refresh

--- updateitem.php
+++ updateitem.php
@@ -24,13 +24,13 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $createDate = $_POST['createDate'];
     $id = $_SESSION['id'];
     $selectedItem = $_POST['selectedItem'];
-
-    $query = "SELECT * FROM `items` WHERE `itemid` = '$selectedItem' AND `user` = '$user' AND `userto` = '$userto'";
+  
+    $query = "SELECT * FROM `items` WHERE `name` = '$selectedItem' AND `user` = '$user' AND `userto` = '$userto'";
     $result = mysqli_query($conn, $query);
 
     if (mysqli_num_rows($result) == 1) {
       // Az itemet a felhasználói adatbázisban frissítjük
-      $query = "UPDATE `items` SET `name`='$name',`status`='$status',`createDate`='$createDate' WHERE `user` = '$user' AND `userto` = '$userto' AND `itemid` = '$selectedItem'";
+      $query = "UPDATE `items` SET `name`='$name',`status`='$status',`createDate`='$createDate' WHERE `user` = '$user' AND `userto` = '$userto' AND `name` = '$selectedItem'";
       $result = mysqli_query($conn, $query);
 
       if ($result) {
