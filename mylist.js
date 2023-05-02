04febe0280472a58ff61207e5ea7e9c68e7d909e
Lehotaiabel <cqde.hu@gmail.com>
2023-05-02 03:06:54 +0200

    test

--- mylist.js
+++ mylist.js
@@ -589,23 +589,24 @@ $(document).on('click', '.invite-card', function (event) {
         return;
     }
 
+    var selectedUser = $.cookie("selectedUser");
+    if (un == selectedUser) {
+        document.title = lastClickedInviteCard.attr("id") + " | " + "GIFTLIST"
+        $("#displayTitle").text(lastClickedInviteCard.attr("id") + "'s list")
+    } else {
+        selectedUser = lastClickedInviteCard.attr("id");
+        document.title = "to " + selectedUser + " | " + "GIFTLIST"
+        $("#displayTitle").text("to " + selectedUser + "'s list")
+    }
+
     addCardBorderi(card);
     $("#listItems").empty();
-    userto = card.attr("id");
-    $.cookie("selectedUser", userto);
-    console.log(un + "//////" + userto);
-    loadItem(selectedUser);
-    
-    if (userto == un) {
-        document.title = userto + " | GIFTLIST";
-        $("#displayTitle").text(userto + "'s list");
-    } else {
-        document.title = "to " + userto + " | GIFTLIST";
-        $("#displayTitle").text("to " + userto + "'s list");
-    }
-    
+    userto = selectedUser;
+    console.log(un + "//////" + userto)
+    loadItem(selectedUser)
     $('#offcanvasMenu').offcanvas('hide');
 });
+
 
 
 
